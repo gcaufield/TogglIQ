563fb654c13ff17bb431e77f639de4b22e7169e0
Greg Caufield <greg@embeddedcoffee.ca>
2020-09-10 18:22:08 -0600

    Setup scripts and configuration for TravisCI
    Add required scripts to build using TravisCI
    
    Issue: #28
    Change-Id: Ie9a69aaeef9c0e7c9668d8bd846ff283f8ab17d2

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# travis.sh script to
+
+SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-lin-3.2.2-2020-08-28-a50584d55.zip"
+SDK_FILE="sdk.zip"
+SDK_DIR="sdk"
+
+PEM_FILE="/tmp/developer_key.pem"
+DER_FILE="/tmp/developer_key.der"
+
+###
+
+wget -O "${SDK_FILE}" "${SDK_URL}"
+unzip "${SDK_FILE}" "bin/*" -d "${SDK_DIR}"
+
+openssl genrsa -out "${PEM_FILE}" 4096
+openssl pkcs8 -topk8 -inform PEM -outform DER -in "${PEM_FILE}" -out "${DER_FILE}" -nocrypt
+
+export MB_HOME="${SDK_DIR}"
+export MB_PRIVATE_KEY="${DER_FILE}"
+
+./mb_runner.sh package
